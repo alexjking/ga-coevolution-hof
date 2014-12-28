dbae9197d3b69d7d944a56401d4996f08a23c624
Alex King <alex@alexjking.co.uk>
2014-12-28 22:23:45 +0000

    Add axis labels and y=50 to the graph.

--- ga/DisplayMutationBias.py
+++ ga/DisplayMutationBias.py
@@ -2,6 +2,7 @@ import Chromosome
 
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 #displays a graph showing the mutation bias within two populations
 class DisplayMutationBias:
@@ -31,21 +32,28 @@ class DisplayMutationBias:
 		print fitness_list_1
 		print fitness_list_2
 
+
+
 if __name__ == '__main__':
 	bias = DisplayMutationBias()
 	x1 = []
 	y1 = []
 	x2 = []
 	y2 = []
+
+	x3 = []
+	y3 = []
+
 	for i in xrange(600):
 		pop1, pop2 = bias.next_generation()
-		for chromosome in pop1:
+		for chromosome in random.sample(pop1, 15):
 			x1.append(i)
 			y1.append(chromosome.get_fitness())
-		for chromosome in pop2:
+		for chromosome in random.sample(pop2, 15):
 			x2.append(i)
 			y2.append(chromosome.get_fitness())
-
+		x3.append(i)
+		y3.append(50)
 		print [c.get_fitness() for c in pop1]
 	plt.scatter(
 		x1,
@@ -61,6 +69,15 @@ if __name__ == '__main__':
 		facecolor='grey',
 		lw=0
 	)
+	plt.scatter(
+		x3,
+		y3,
+		s=1,
+		facecolor='black',
+		lw=1
+	)
+	plt.ylabel('Objective fitness')
+	plt.xlabel('Generations')
 	plt.axis([0, 600, 0, 100])
 	plt.show()
 		#bias.print_populations()
